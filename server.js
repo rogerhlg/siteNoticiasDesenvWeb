ad44868db95abacbd5f876869b6b51986e848f59
rogerhlg <rogerhlg@outlook.com>
2021-06-14 09:57:34 -0300

    commitando arquivos principais pré-renderização pelo lado cliente

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+const { request } = require('http');
+var path = require('path');
+
+var app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.use(express.static(path.join(__dirname, './static')))
+
+const dados = require('./dados/noticias.json');
+
+const noticia = {
+    titulo: "Rodovia de US$ 1 bilhão não leva a lugar nenhum em Montenegro (e pode quebrar o país europeu)",
+    resumo: "Pequena nação nos Bálcãs, que tenta entrar na União Europeia, pegou empréstimo da China e ainda não concluiu trecho de apenas 41 km. Conselho Europeu alerta para 'armadilha da dívida'.",
+    categoria: "Infraestrutura",
+    foto: "alface.png"
+}
+
+app.get(['/', '/index', '/home'], (req, res) => {
+    res.render("index", { noticias: dados });
+});
+
+app.get("/noticias", (req, res) => {
+    let id = req.params.id;
+    let noticia = dados[id];
+    res.render("noticias", {noticias: dados});
+ });
+
+app.get("/noticia/:id", (req, res) => {
+    let id = req.params.id;
+    let noticia = dados[id];
+    res.render("noticia", {noticia: noticia});
+ });
+
+app.listen(8000, function() {
+    console.log('Servidor rodando!')
+});
+
+
+function get(name){
+    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+       return decodeURIComponent(name[1]);
+ }
